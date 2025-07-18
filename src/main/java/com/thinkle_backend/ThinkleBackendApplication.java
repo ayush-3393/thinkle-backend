d9a3fb084da9b5c59c0bf38ff88a623da4150a84
Ayush Sinha <ayush.sinha3393@gmail.com>
2025-07-19 02:37:00 +0530

    project initialized

---
+++ src/main/java/com/thinkle_backend/ThinkleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.thinkle_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkleBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thinkle_backend/ThinkleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thinkle_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
